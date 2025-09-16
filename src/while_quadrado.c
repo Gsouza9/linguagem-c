fd4877e1a42b2af274c718038a84b2ba836bbc91
senac <senac@localhost.localdomain>
2025-09-16 14:34:16 -0300

    Estudo da estrutura while

---
+++ src/while_quadrado.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    system("clear");
+    int lin=1, col=1;
+
+    while (lin <=10){
+        col = 0;
+        while (col <=20){
+            printf(" te♡amo");
+            col++;
+        }
+        printf("\n");
+        lin++;
+    }
+    
+    return 0;
+}
